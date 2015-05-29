276d491b28d9192217af19e47a35a887f2846906
Matt Hova <matthova87@gmail.com>
2015-05-29 17:42:02 +0000

    updated camera resolution

--- timelapse.py
+++ timelapse.py
@@ -1,13 +1,14 @@
 import time
 import picamera
 import os
-time_between_photos = 60 * 60 # one photo per hour
+time_between_photos = 60 * 60# one photo per hour
 
 directory = os.getcwd() + "/photos/"
 if not os.path.exists(os.getcwd() + "/photos"):
 	os.makedirs(directory)
 
 camera = picamera.PiCamera()
+camera.resolution = (2592, 1944)
 
 while True:
 	filename = directory + "image " + str(int(time.time())) + ".jpg"
